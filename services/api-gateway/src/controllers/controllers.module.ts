f6c7dc97a1a4caea15aea3828105370b065a0af3
devsinglad <simonsinglad5@gmail.com>
2025-11-10 11:54:10 +0100

    Remove user-service module and related files
    - Deleted package.json, main application files, and utility functions for user-service.
    - Removed all controller, service, and module files associated with user-service.
    - Cleared out test files and configurations related to user-service.

--- services/api-gateway/src/controllers/controllers.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HealthController } from './health/health.controller';
-import { AppService } from 'src/app.service';
-
-@Module({
-  controllers: [HealthController],
-  providers: [AppService],
-})
-export class ControllersModule { }
